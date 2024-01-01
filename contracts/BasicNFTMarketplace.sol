524437e9339b7f2384ecf3029872f8db4d50e607
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2024-01-01 22:18:51 +0000

    created functions

--- contracts/BasicNFTMarketplace.sol
+++ contracts/BasicNFTMarketplace.sol
@@ -3,13 +3,23 @@
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-//import "@openzeppelin/contracts/utils/Counters.sol";
-contract BasicNFTMarketplace is ERC721URIStorage {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+ struct NFTListing {
+        uint256 price;
+        address seller;
+    }
+
+contract BasicNFTMarketplace is ERC721URIStorage, Ownable(msg.sender) {
 
     //using Counters for Counters.Counter;
     uint256 private _tokenIDs = 0;
- 
-    constructor() ERC721("Basic NFT", "BNFT") {}
+    mapping(uint256 => NFTListing) private _listing;
+    event NFTTransfer(uint256 tokenID, address to, string tokenURI, uint256 price);
+
+    constructor() ERC721("Basic NFT", "BNFT") {
+        
+    }
 
     function createNFT(string calldata tokenURI) public returns(uint256) {
         //_tokenIDs.increment();
@@ -17,6 +27,46 @@ contract BasicNFTMarketplace is ERC721URIStorage {
         _tokenIDs++;
         _safeMint(msg.sender, _tokenIDs);
         _setTokenURI(_tokenIDs, tokenURI);
+        emit NFTTransfer(_tokenIDs, msg.sender, tokenURI, 0);
         return _tokenIDs;
     }
+
+    function listNFT(uint256 tokenID, uint256 price) public {
+        require(price > 0, "Price must be greater than zero to list NFT");
+        transferFrom(msg.sender, address(this), tokenID);
+        _listing[tokenID] = NFTListing(price, msg.sender);
+        emit NFTTransfer(tokenID, address(this), "", price);
+    }
+
+    function buyNFT(uint256 tokenID) public payable {
+        NFTListing memory listing = _listing[tokenID];
+        require(listing.price > 0, "NFT not listed for sale");
+        require(msg.value == listing.price, "incorrect price");
+        transferFrom(address(this), msg.sender, tokenID);
+        clearListing(tokenID);
+        payable(msg.sender).transfer(listing.price * 95 / 100);
+        emit NFTTransfer(tokenID, msg.sender, "", 0);
+    }
+
+    function cancelListing(uint256 tokenID) public {
+        NFTListing memory listing = _listing[tokenID];
+        require(listing.price > 0, "NFT not listed for sale");
+        require(listing.seller == msg.sender, "you're not the owner");
+        transferFrom(address(this), msg.sender, tokenID);
+        clearListing(tokenID);
+        emit NFTTransfer(tokenID, msg.sender, "", 0);
+    }
+
+    function withdrawFunds() public onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No funds to withdraw");
+        payable(owner()).transfer(balance);
+    }
+
+    function clearListing(uint256 tokenID) public {
+        _listing[tokenID].price = 0;
+        _listing[tokenID].seller = address(0);
+    }
+
+
 }
--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,16 @@
 import { ethers } from "hardhat";
+import { expect } from "chai";
+import { token } from "../typechain-types/@openzeppelin/contracts";
+import { Contract } from "hardhat/internal/hardhat-network/stack-traces/model";
 
+describe("DeployBasicNFTMarketplace", () => {
+    let basicNftMarketplace: Contract;
+
+    before(async () => {
+        const basicNFTMarketplace = await ethers.getContractFactory("BasicNFTMarketplace");
+        const basicNftMarketplace = await basicNFTMarketplace.deploy();
+    });
+});
 
 describe("BasicNFTMarketplace", () => {
     it("Should do something", async () => {
@@ -7,9 +18,19 @@ describe("BasicNFTMarketplace", () => {
       const basicNftMarketplace = await basicNFTMarketplace.deploy();
       //await basicNftMarketplace.deployed();
 
-      const tokenURI = "";
+      const tokenURI = "https://some-token.uri/";
       const transaction = await basicNftMarketplace.createNFT(tokenURI);
-      console.log(transaction);
       const receipt = await transaction.wait();
+      
+      /*
+      const tokenID = receipt.events[0].args.tokenId;
+      const mintedTokenURI = await basicNftMarketplace.tokenURI(tokenID);
+      expect(mintedTokenURI).to.equal(tokenURI);
+
+      const ownerAddress = await basicNftMarketplace.ownerOf(tokenID);
+      const signers = await ethers.getSigners();
+      const currentAddress = await signers[0].getAddress();
+      expect(ownerAddress).to.equal(currentAddress);
+      */
     });
 });
